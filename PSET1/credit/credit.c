fabe0314a48e27b180d2892d3b87baf6d369b05b
Andrew Muncie <muncie88@hotmail.com>
2022-11-23 19:54:10 +0000

    Credit style50 & checksum logic debug

--- PSET1/credit/credit.c
+++ PSET1/credit/credit.c
@@ -4,25 +4,23 @@
 int main(void) 
 {
     long number = get_long("Number: ");  
-    int length = 0;
+    int digit_counter = 0;
     int digit = 0;
     int following_digit = 0;
-    int doubled_digit = digit * 2;
     int sum = 0;
-    int checksum = (sum % 10);
 
     while (number > 0)
     {
-        length ++;
-        if ((length % 2 == 0))  // every 2nd digit (in reverse)
+        digit_counter ++;
+        if ((digit_counter % 2 == 0))  // digit's *order* (in reverse) is even
         {
             following_digit = digit;
             digit = number % 10;
-
-            for (int i = 0; i < 2; i++) // loops over both 'numerals' and 'tens' columns
+            int doubled_digit = digit * 2;
+            while (doubled_digit > 0)    // loops over 'units' and 'tens' columns...
             {
                 sum = sum + (doubled_digit % 10);
-                doubled_digit = doubled_digit / 10;
+                doubled_digit = doubled_digit / 10; // integer division effects removal of last digit
             }
         }
         else // every odd numbered digit (in reverse)
@@ -31,39 +29,34 @@ int main(void)
             digit = number % 10;
             sum = sum + digit;
         }
-        number = (number / 10); // integer division effects removal of last digit
+        number = (number / 10); 
     }
 
-  if ((checksum == 0) &&
-    (length == 15) &&
-    (digit == 3) && (following_digit == 4 || following_digit == 7))
-  {
-    printf ("AMEX\n");
-  }
+    int checksum = (sum % 10);
 
-  else if ((checksum == 0) &&
-    (length == 16) && (digit == 5) &&
-    (following_digit > 0) && (following_digit < 6))
-  {
-    printf ("MASTERCARD\n");
-  }
+    if ((checksum == 0) &&
+        (digit_counter == 15) &&
+        (digit == 3) && (following_digit == 4 || following_digit == 7))
+    {
+        printf("AMEX\n");
+    }
 
-  else if ((checksum == 0) &&
-    (length == 13 || length == 16) &&
-    (digit == 4))
-  {
-    printf ("VISA\n");
-  }
+    else if ((checksum == 0) &&
+             ((digit_counter == 16) && (digit == 5)) &&
+             (following_digit > 0) && (following_digit < 6))
+    {
+        printf("MASTERCARD\n");
+    }
 
-  else
-    printf ("INVALID\n");
-
-  // test comment
+    else if ((checksum == 0) &&
+             (digit_counter == 13 || digit_counter == 16) &&
+             (digit == 4))
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+    
 }
-
-
-
-
-
-
-
